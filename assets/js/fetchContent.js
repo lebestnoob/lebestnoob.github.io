7ecc2bb6788dfc2b597b09da0d2cd8ffb71983cd
lebestnoob <lebestnoob12@gmail.com>
2026-06-20 22:54:47 -0700

    works, needs validation for non-existent hashes

--- assets/js/fetchContent.js
+++ assets/js/fetchContent.js
@@ -9,7 +9,11 @@ window.onload = function(){
         for (var i=0; i < keys.length; i++){   
             
             var result = fetchContent("/templates/" + keys[i]);
-            var path = window.location.pathname;
+            var path = window.location.hash.substring(1, window.location.hash.length);
+
+            if (window.location.pathname != "/")  { 
+                path = "404.html";
+            } 
             
             if(keys[i] == "main.html")
                 result = fetchContent("/pages/" + path);
@@ -17,8 +21,21 @@ window.onload = function(){
             document.getElementById(templatesList[keys[i]].id).innerHTML = result;
             
         }
-
     }
+
+    var hrefs = document.getElementsByTagName("a");
+
+    for(var i=0; i < hrefs.length; i++){
+        if(hrefs[i].href.includes("#")) {
+            hrefs[i].onclick = function(){
+                                window.location.hash = this.href;
+                                setTimeout(function() {
+                                    window.location.reload();
+                                }, 100);
+                            }
+        }
+    }
+
 }
 
 function fetchContent(url){
