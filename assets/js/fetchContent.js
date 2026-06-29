5771edafc6864d4a6d36123ee0560281f308d316
lebestnoob <lebestnoob12@gmail.com>
2026-06-28 19:33:05 -0700

    Basic markdown support: missing tables, partial block quotes and lists

--- assets/js/fetchContent.js
+++ assets/js/fetchContent.js
@@ -1,5 +1,6 @@
 window.onload = function() {
-        fetchContent("/templates/assembly.json", function(content) {
+        
+    fetchContent("/templates/assembly.json", function(content) {
             var templatesList;            
             try {
                 templatesList = JSON.parse(content);
@@ -88,7 +89,12 @@ function loadContent(){
     var path = window.location.pathname != "/" ? "404.html" : window.location.hash.substring(1, window.location.hash.length);
     var main = document.getElementById("main");
     fetchContent("/pages/" + path, function(pageResult) {
-        main.innerHTML = pageResult;
+        var result = pageResult;
+        if (path.endsWith(".md")) {
+            result = mdtoHTML(pageResult)
+        }
+        
+        main.innerHTML = result;
         document.title = main.getElementsByTagName("h1")[0].innerText;
 
         if (main.getElementsByTagName("p")[0]) {
@@ -103,6 +109,91 @@ function loadContent(){
         document.title = document.title != "lebestnoob" ? document.title + " - lebestnoob" : document.title;
     }
 )}
+
+
+function mdtoHTML(str) {
+    // Regex from https://gist.github.com/elfefe/ef08e583e276e7617cd316ba2382fc40
+    var headerRegex = /^(#{1,6})\s+(.+)$/gm;
+    var boldRegex = /\*\*(.+?)\*\*|__(.+?)__/gm;
+    var italicRegex = /\*(.+?)\*|_(.+?)_/gm;
+    var strikeThroughRegex = /~~(.+?)~~/gm;
+    var linkRegex = /\[(.*?)\]\((.*?)\s?(?:"(.*?)")?\)/gm;
+    var imageRegex = /!\[(.*?)\]\((.*?)\s?(?:"(.*?)")?\)/gm;
+    var codeBlockRegex = /^\`\`\`(?:\s*(\w+))?([\s\S]*?)^\`\`\`$/gm;
+    var codeRegex = /`(.+?)`/gm;
+
+    var blockQuoteRegex = /^>\s*(.+)$/gm; // single-level quotes only
+    var unorderedListRegex = /^\s*[-+*]\s+(.+)$/gm;
+    var orderedListRegex = /^\s*\d+\.\s+(.+)$/gm;
+
+    
+    var paragraphRegex = /^([^#].*)|\n{2,}/g;
+    
+    var final = str;
+    
+    final = final.replace(codeBlockRegex, function(match,p1,p2){
+        return "<pre><code>" + p1 + p2 + "</code></pre>";
+    })
+
+    final = final.replace(blockQuoteRegex, function(match,p1) {
+        return "<blockquote>" + p1 + "</blockquote>";
+    })
+
+    final = final.replace(unorderedListRegex, function(match, p1) {
+        return "<ul><li>" + p1 + "</li></ul>";
+    })
+    final = final.replaceAll("</li></ul>\n<ul><li>", "</li><li>")
+
+
+    final = final.replace(orderedListRegex, function(match, p1) {
+        return "<ol><li>" + p1 + "</li></ol>";
+    })
+    final = final.replaceAll("</li></ol>\n<ol><li>", "</li><li>")
+
+    var chunks = final.split(paragraphRegex);
+    var processedArr = [];
+    for(var l=0; l<chunks.length; l++){
+        if(chunks[l]==undefined){
+            continue;
+        }
+        if(chunks[l].startsWith("#") || chunks[l].startsWith("`")) {
+            processedArr.push(chunks[l]);
+            continue;
+        }
+        processedArr.push("<p>" + chunks[l] + "</p>");
+    }
+    final = processedArr.join("\n\n");
+    
+    final = final.replace(headerRegex, function(match, p1, p2){
+        return "<h"+p1.length+">"+p2+"</h"+p1.length+">";
+    })
+    
+    final = final.replace(boldRegex, function(match, p1){
+        return "<b>"+p1+"</b>";
+    })
+    
+    final = final.replace(italicRegex, function(match, p1){
+        return "<i>"+p1+"</i>";
+    })
+    
+    final = final.replace(strikeThroughRegex, function(match, p1){
+        return "<s>"+p1+"</s>";
+    })
+
+    final = final.replace(imageRegex, function(match, p1, p2){
+        return "<img src=\""+ p2 +"\" alt=\"" + p1 + "\">";
+    })
+    
+    final = final.replace(linkRegex, function(match, p1, p2){
+        return "<a href=\"" + p2 + "\">" + p1 + "</a>";
+    })
+
+    final = final.replace(codeRegex, function(match,p1,p2){
+        return "<code>" + p1 + "</code>";
+    })
+
+    return final;
+}
 
 if (!String.prototype.startsWith) {
     String.prototype.startsWith = function(searchString, position){
---
+++ pages/my-markdown.md
@@ -0,0 +1,46 @@
+# header 1
+## header 2
+### header 3
+#### header 4
+##### header 5
+###### header 6
+
+Regular text.
+
+Here is some **bold text**
+
+Now it's *italicized*
+
+Let's make it ~~strikethrough~~
+
+Here is a [hyperlink](https://google.com)
+
+Image:
+
+![Minion](https://octodex.github.com/images/minion.png)
+
+Code Block
+
+```js
+here goes some really long code
+multiline
+another line
+```
+
+My `inline code` is so nice, isn't it?
+
+Block Quote:
+
+> The fox
+
+Unordered List:
+
+* Item 1
+* Item 3
+* Item 2
+
+Ordered List:
+
+1. Item 1
+2. Item 2
+3. Item 3
